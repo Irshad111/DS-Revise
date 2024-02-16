7064bde11c5ffada8f315f538696bccac5b836a3
Irshad111 <irshadkhan1798@gmail.com>
2024-02-16 09:45:12 +0530

    KMP

--- src/string/StringQ.java
+++ src/string/StringQ.java
@@ -97,7 +97,105 @@ public class StringQ {
 
 	}
 
+	// 6.  manachers algo to count all palindromic algo or longest palindroic algo
+	public static int manachers(String str) {
+		int max=0;
+		//odd
+		for(int axis = 0; axis < str.length(); axis++){
+			for(int orb = 0; (axis - orb) >= 0 && (axis + orb) < str.length(); orb++){
+				if(str.charAt(axis - orb) == str.charAt(axis + orb)){
+					max=Math.max(max, 2 * orb + 1);
+				}else{
+					break;
+				}
+			}
+		}
+		//even
+		for(double axis = 0.5; axis < str.length(); axis++){
+			for(double orb = 0.5; (axis - orb) >= 0 && (axis + orb) < str.length(); orb++){
+				if(str.charAt((int)(axis - orb)) == str.charAt((int)(axis + orb))){
+					max=Math.max(max, (int)(2 * orb));
+				}else{
+					break;
+				}
+			}
+		}
+		return max;
+	}
 
+	// 7. KMP
+	/**
+	 * Slow method of pattern matching
+	 */
+	public boolean hasSubstring(char[] text, char[] pattern){
+		int i=0;
+		int j=0;
+		int k = 0;
+		while(i < text.length && j < pattern.length){
+			if(text[i] == pattern[j]){
+				i++;
+				j++;
+			}else{
+				j=0;
+				k++;
+				i = k;
+			}
+		}
+		if(j == pattern.length){
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Compute temporary array to maintain size of suffix which is same as prefix
+	 * Time/space complexity is O(size of pattern)
+	 */
+	private int[] computeTemporaryArray(char pattern[]){
+		int [] lps = new int[pattern.length];
+		int index =0;
+		for(int i=1; i < pattern.length;){
+			if(pattern[i] == pattern[index]){
+				lps[i] = index + 1;
+				index++;
+				i++;
+			}else{
+				if(index != 0){
+					index = lps[index-1];
+				}else{
+					lps[i] =0;
+					i++;
+				}
+			}
+		}
+		return lps;
+	}
+
+	/**
+	 * KMP algorithm of pattern matching.
+	 */
+	public boolean KMP(char []text, char []pattern){
+
+		int lps[] = computeTemporaryArray(pattern);
+		int i=0;
+		int j=0;
+		while(i < text.length && j < pattern.length){
+			if(text[i] == pattern[j]){
+				i++;
+				j++;
+			}else{
+				if(j!=0){
+					j = lps[j-1];
+				}else{
+					i++;
+				}
+			}
+		}
+		if(j == pattern.length){
+			return true;
+		}
+		return false;
+	}
 
 
 }
